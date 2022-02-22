aa0bda2295b107b76632a36b9e00be9052919ef1
fabriciomsantos <37518637+fabriciomsantos@users.noreply.github.com>
2022-02-22 00:17:00 -0300

    Updated delegates

--- Source/GameplayTagSystem/Public/Delegates/DynamicDelegates.h
+++ Source/GameplayTagSystem/Public/Delegates/DynamicDelegates.h
@@ -17,29 +17,29 @@ DECLARE_DYNAMIC_DELEGATE_OneParam(FIntDelegate, int32, Value);
 
 DECLARE_DYNAMIC_DELEGATE_OneParam(FFloatDelegate, float, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FNameDelegate,const FName&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FNameDelegate, FName, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FStringDelegate, const FString&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FStringDelegate, FString, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FTextDelegate, const FText&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FTextDelegate, FText, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FVectorDelegate, const FVector&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FVectorDelegate, FVector, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FRotatorDelegate, const FRotator&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FRotatorDelegate, FRotator, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FTransformDelegate, const FTransform&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FTransformDelegate, FTransform, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FObjectDelegate, const UObject*&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FObjectDelegate, UObject*, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FActorDelegate, const AActor*&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FActorDelegate, AActor*, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FPawnDelegate, const APawn*&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPawnDelegate, APawn*, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FCharacterDelegate, const ACharacter*&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FCharacterDelegate, ACharacter*, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FActorComponentDelegate, const UActorComponent*&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FActorComponentDelegate, UActorComponent*, Value);
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FSceneComponentDelegate, const USceneComponent*&, Value);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FSceneComponentDelegate, USceneComponent*, Value);
 
 #pragma endregion
 
@@ -87,69 +87,69 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FVoidMulticastDelegate);
 
 #pragma region Variable
 
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FBoolMulticastDelegate, bool, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIntMulticastDelegate, int32, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFloatMulticastDelegate, float, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNameMulticastDelegate, FName, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStringMulticastDelegate, FString, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTextMulticastDelegate, FText, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FVectorMulticastDelegate, FVector, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRotatorMulticastDelegate, FRotator, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTransformMulticastDelegate, FTransform, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FObjectMulticastDelegate, UObject*, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorMulticastDelegate, AActor*, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPawnMulticastDelegate, APawn*, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterMulticastDelegate, ACharacter*, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorComponentMulticastDelegate, UActorComponent*, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSceneComponentMulticastDelegate, USceneComponent*, Value);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FBoolMulticastDelegate, bool, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIntMulticastDelegate, int32, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFloatMulticastDelegate, float, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNameMulticastDelegate, FName, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStringMulticastDelegate, FString, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTextMulticastDelegate, FText, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FVectorMulticastDelegate, FVector, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRotatorMulticastDelegate, FRotator, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTransformMulticastDelegate, FTransform, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FObjectMulticastDelegate, UObject*, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorMulticastDelegate, AActor*, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPawnMulticastDelegate, APawn*, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterMulticastDelegate, ACharacter*, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorComponentMulticastDelegate, UActorComponent*, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSceneComponentMulticastDelegate, USceneComponent*, Value);
 
 #pragma endregion
 
 #pragma region Array
 
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FBoolArrayMulticastDelegate, const TArray<bool>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIntArrayMulticastDelegate, const TArray<int32>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFloatArrayMulticastDelegate, const TArray<float>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNameArrayMulticastDelegate, const TArray<FName>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStringArrayMulticastDelegate, const TArray<FString>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTextArrayMulticastDelegate, const TArray<FText>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FVectorArrayMulticastDelegate, const TArray<FVector>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRotatorArrayMulticastDelegate, const TArray<FRotator>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTransformArrayMulticastDelegate, const TArray<FTransform>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FObjectArrayMulticastDelegate, const TArray<UObject*>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorArrayMulticastDelegate, const TArray<AActor*>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPawnArrayMulticastDelegate, const TArray<APawn*>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterArrayMulticastDelegate, const TArray<ACharacter*>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorComponentArrayMulticastDelegate, const TArray<UActorComponent*>&, Value);
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSceneComponentArrayMulticastDelegate, const TArray<USceneComponent*>&, Value);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FBoolArrayMulticastDelegate, const TArray<bool>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIntArrayMulticastDelegate, const TArray<int32>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFloatArrayMulticastDelegate, const TArray<float>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNameArrayMulticastDelegate, const TArray<FName>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStringArrayMulticastDelegate, const TArray<FString>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTextArrayMulticastDelegate, const TArray<FText>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FVectorArrayMulticastDelegate, const TArray<FVector>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRotatorArrayMulticastDelegate, const TArray<FRotator>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTransformArrayMulticastDelegate, const TArray<FTransform>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FObjectArrayMulticastDelegate, const TArray<UObject*>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorArrayMulticastDelegate, const TArray<AActor*>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPawnArrayMulticastDelegate, const TArray<APawn*>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterArrayMulticastDelegate, const TArray<ACharacter*>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorComponentArrayMulticastDelegate, const TArray<UActorComponent*>&, Value);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSceneComponentArrayMulticastDelegate, const TArray<USceneComponent*>&, Value);
 
 #pragma endregion
 
